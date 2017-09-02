d1945ee243cd844e6f3076e42fa7b36c3f285893
Justin Toniazzo <jutonz42@gmail.com>
2017-09-02 17:29:31 -0400

    lego

--- config/prod.exs
+++ config/prod.exs
@@ -24,43 +24,18 @@ config :homepage, Homepage.Web.Endpoint,
 # Do not print debug messages in production
 config :logger, level: :info
 
-# ## SSL Support
-#
-# To get SSL working, you will need to add the `https` key
-# to the previous section and set your `:url` port to 443:
-#
-#     config :homepage, Homepage.Endpoint,
-#       ...
-#       url: [host: "example.com", port: 443],
-#       https: [port: 443,
-#               keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#               certfile: System.get_env("SOME_APP_SSL_CERT_PATH")]
-#
-# Where those two env variables return an absolute path to
-# the key and cert in disk or a relative path inside priv,
-# for example "priv/ssl/server.key".
-#
-# We also recommend setting `force_ssl`, ensuring no data is
-# ever sent via http, always redirecting to https:
-#
-#     config :homepage, Homepage.Endpoint,
-#       force_ssl: [hsts: true]
-#
-# Check `Plug.SSL` for all available options in `force_ssl`.
+secret_key_base = System.get_env "SECRET_KEY_BASE"
+config :homepage, Homepage.Endpoint, secret_key_base: secret_key_base
 
-# ## Using releases
-#
-# If you are doing OTP releases, you need to instruct Phoenix
-# to start the server for all endpoints:
-#
-#     config :phoenix, :serve_endpoints, true
-#
-# Alternatively, you can configure exactly which server to
-# start per endpoint:
-#
-#     config :homepage, Homepage.Endpoint, server: true
-#
+db_host = System.get_env("PGHOST") || "psql"
+db_user = System.get_env "PGUSER"
+db_pass = System.get_env "PGPASS"
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
+# Configure your database
+config :homepage, Homepage.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  hostname: db_host,
+  username: db_user,
+  password: db_pass,
+  database: "homepage_prod",
+  pool_size: 20
