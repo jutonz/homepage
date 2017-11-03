d86d7466104778211100e54333cbd23698893114
Justin Toniazzo <jutonz42@gmail.com>
2017-11-03 11:25:17 -0400

    Adds periodic db backups
    Posts backup to s3 hourly.

--- docker/prod/psql/backup.sh
+++ docker/prod/psql/backup.sh
@@ -1,7 +1,15 @@
 #/bin/bash
 
-pg_dump --dbname=homepage_prod --username=homepage -Fc > /tmp/homepage_prod.dump
+PGPASSWORD=$POSTGRES_PASSWORD \
+  pg_dump \
+  --host=psql \
+  --dbname=$POSTGRES_DB \
+  --username=$POSTGRES_USER \
+  -Fc \
+  > /tmp/homepage_prod.dump
 
-aws s3 cp /tmp/homepage_prod.dump s3://jutonz-homepage-prod-db-backups/homepage_prod.dump
+aws s3 cp \
+  /tmp/homepage_prod.dump \
+  s3://jutonz-homepage-prod-db-backups/homepage_prod.dump
 
 rm /tmp/homepage_prod.dump
