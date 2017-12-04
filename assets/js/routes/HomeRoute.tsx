de173bde3e256a54386aa8ed7c2801c77d0a4f22
Justin Toniazzo <jutonz42@gmail.com>
2017-12-04 08:50:41 -0500

    Fix typo

--- assets/js/routes/HomeRoute.tsx
+++ assets/js/routes/HomeRoute.tsx
@@ -1,20 +1,13 @@
 import * as React from 'react';
 import { MainNav, ActiveItem } from './../components/MainNav';
 import { Incr } from './../components/Incr';
-import { asAuthenticatedRoute } from './asAuthenticatedRoute';
 
-class _HomeRoute extends React.Component {
-  public render() {
-    return (
-      <div>
-        <MainNav activeItem={ActiveItem.Home} />
-        <Incr />
-        <Incr />
-        <Incr />
-        <Incr />
-      </div>
-    );
-  }
-}
-
-export const HomeRoute = asAuthenticatedRoute(_HomeRoute);
+export const HomeRoute = () => (
+  <div>
+    <MainNav activeItem={ActiveItem.Home} />
+    <Incr />
+    <Incr />
+    <Incr />
+    <Incr />
+  </div>
+);
--- assets/js/routes/SettingsRoute.tsx
+++ assets/js/routes/SettingsRoute.tsx
@@ -2,13 +2,9 @@ import * as React from 'react';
 import { MainNav, ActiveItem } from './../components/MainNav';
 import { ChangePasswordForm } from './../components/ChangePasswordForm';
 
-export class SettingsRoute extends React.Component {
-  public render() {
-    return (
-      <div>
-        <MainNav activeItem={ActiveItem.Settings}/>
-        <ChangePasswordForm />
-      </div>
-    );
-  }
-}
+export const SettingsRoute = () => (
+  <div>
+    <MainNav activeItem={ActiveItem.Settings}/>
+    <ChangePasswordForm />
+  </div>
+);
