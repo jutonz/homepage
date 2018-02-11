4db2058421e4230785d5ae600ea7ac433fc5b65a
Justin Toniazzo <jutonz42@gmail.com>
2018-02-10 22:22:34 -0500

    smarter backup script

--- docker/ci/psql/initdb.sh
+++ docker/ci/psql/initdb.sh
@@ -10,13 +10,11 @@ res=`aws s3api head-object \
   --bucket jutonz-homepage-prod-db-backups \
   --key ci_prod.dump` || true
 
-if [ -z "$res" ]; then
-  echo "db backup is not presnt on s3. Skipping restore."
-  exit 0
+if [ -n "$res" ]; then
+  aws s3 cp s3://jutonz-homepage-prod-db-backups/ci_prod.dump ci_prod.dump
+
+  # pg_restore returns nonzero code since some imports don't work. This is okay,
+  # so return true to keep container running (nonzero will kill it)
+  pg_restore -d $POSTGRES_DB --role=$POSTGRES_USER ci_prod.dump || true
 fi
 
-aws s3 cp s3://jutonz-homepage-prod-db-backups/ci_prod.dump ci_prod.dump
-
-# pg_restore returns nonzero code since some imports don't work. This is okay,
-# so return true to keep container running (nonzero will kill it)
-pg_restore -d $POSTGRES_DB --role=$POSTGRES_USER ci_prod.dump || true
