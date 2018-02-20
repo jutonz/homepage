6c2342571f9232d2c352425865cb8751e821af2f
Justin Toniazzo <jutonz42@gmail.com>
2018-02-19 19:41:27 -0500

    blah

--- docker/dev/app/seedci.sh
+++ docker/dev/app/seedci.sh
@@ -6,10 +6,13 @@ set -ex
 
 cd /tmp/code
 
+ls -alh /tmp
+exit 1
+
 #cp -r /tmp/code/deps deps
 #cp -r /tmp/code/_build _build
-mix deps.get
-ls -alh
+#mix deps.get
+#ls -alh
 
 mix ecto.create --force
 mix ecto.migrate
