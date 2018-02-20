d014ed6c81f3fb1bf51d77ef4a8614642577ae32
Justin Toniazzo <jutonz42@gmail.com>
2018-02-19 20:05:44 -0500

    blah

--- docker/dev/psql/initdb.sh
+++ docker/dev/psql/initdb.sh
@@ -3,6 +3,7 @@ set -e
 
 chown -Rf postgres:postgres /var/lib/postgresql/data
 chmod -R 700 /var/lib/postgresql/data
+rm -rf /var/lib/postgresql/data/*
 
 su - postgres -c "/usr/lib/postgresql/10/bin/initdb -D /var/lib/postgresql/data/ --encoding=utf8"
 
