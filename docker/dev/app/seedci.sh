6eabfe0929bf59c6e45a10127055b3f7be91c323
Justin Toniazzo <jutonz42@gmail.com>
2018-02-19 19:34:25 -0500

    copy deps

--- docker/dev/app/seedci.sh
+++ docker/dev/app/seedci.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -ex
+
 # Idential to seed.sh, but moves to /tmp/code rather than /root to find app files.
 
-set -x
-set -e
-
 cd /tmp/code
+
+cp -r /tmp/code/deps deps
+cp -r /tmp/code/_build _build
 ls -alh
 
 mix ecto.create --force
