b91ab00beaf3534aa1fe2a06e2f33bfa044ae024
Justin Toniazzo <jutonz42@gmail.com>
2018-02-25 11:31:28 -0500

    explicit compile

--- docker/dev/app/runtests.sh
+++ docker/dev/app/runtests.sh
@@ -4,9 +4,12 @@ tree apps/redis
 
 cd apps/client/assets && yarn lint && cd -
 
+
+MIX_ENV=test mix compile
+mix test
 # A bug with ecto currently breaks tests the first time they are run for an
 # umbrella app (something to do with migrations being run multiple times?). It
 # succeeds the second time, though, so just ignore the output and exit status
 # of the first round.
-/bin/bash -c "mix test; true" >> /dev/null
-mix test
+#/bin/bash -c "mix test; true" >> /dev/null
+#mix test
