4c5aee3c795032787831a30363d5e8285bb92382
Justin Toniazzo <jutonz42@gmail.com>
2018-03-05 09:51:10 -0500

    one more script

---
+++ docker/ci/testimage/get-live-images.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+set -e
+
+service="app"
+
+echo $KUBELET_CONF | base64 -d > $KUBECONFIG
+
+dctl tag-for $service
+dctl k8s live-image $service -n homepage
+
+if dctl k8s is-outdated $service -n homepage; then
+  echo "$service is outdated"
+else
+  echo "$service is not outdated"
+fi
+
+
+rm -f $KUBECONFIG
