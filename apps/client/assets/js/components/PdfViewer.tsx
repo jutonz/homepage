0ba43818c273f70803e505b5fde1c4f35fe413e4
Justin Toniazzo <jutonz42@gmail.com>
2018-03-07 17:07:25 -0500

    Rescale resume on page resize (#21)

--- apps/client/assets/js/components/PdfViewer.tsx
+++ apps/client/assets/js/components/PdfViewer.tsx
@@ -9,6 +9,7 @@ interface IProps {
 
 interface IState {
   canvasId: string;
+  page?: PDFPageProxy;
 }
 
 class _PdfViewer extends React.Component<IProps, IState> {
@@ -28,29 +29,48 @@ class _PdfViewer extends React.Component<IProps, IState> {
 
   private onRenderSuccess = (pdf: PDFDocumentProxy) => {
     pdf.getPage(1).then((page: PDFPageProxy) => {
-      console.log("Page loaded");
-      const scale = 1.5;
-      const viewport = page.getViewport(scale);
-
-      // Prepare canvas using PDF page dimensions
-      const canvasId = this.state.canvasId;
-      const canvas = document.getElementById(canvasId) as HTMLCanvasElement;
-      const context = canvas.getContext("2d");
-      canvas.height = viewport.height;
-      canvas.width = viewport.width;
-
-      // Render PDF page into canvas context
-      const renderContext = {
-        canvasContext: context,
-        viewport: viewport
-      };
-
-      const renderTask = page.render(renderContext);
-      renderTask.then(function() {
-        console.log("Page rendered");
-      });
+      this.renderPage(page);
     });
   };
+
+  public componentDidMount() {
+    window.addEventListener("resize", () => {
+      const page = this.state.page;
+      if (page) {
+        this.renderPage(page); // rescale
+      }
+    });
+  }
+
+  private renderPage(page: PDFPageProxy) {
+    this.setState({ page });
+    console.log("Page loaded");
+    const canvasId = this.state.canvasId;
+    const canvas = document.getElementById(canvasId) as HTMLCanvasElement;
+
+    const scale = this.optimumScale(page, canvas);
+    console.log("optimum scale is", scale);
+    const viewport = page.getViewport(scale);
+
+    // Prepare canvas using PDF page dimensions
+    const context = canvas.getContext("2d");
+    const { height, width } = viewport;
+    canvas.height = height;
+    canvas.width = width;
+
+    // Render PDF page into canvas context
+    const renderContext = {
+      canvasContext: context,
+      viewport: viewport
+    };
+
+    const renderTask = page.render(renderContext);
+    renderTask.then(() => console.log("Page rendered"));
+  }
+
+  private optimumScale(page: PDFPageProxy, canvas: HTMLCanvasElement) {
+    return canvas.clientHeight / page.getViewport(1.0).height;
+  }
 
   private onRenderFailure = (reason: string) => {
     console.error(reason);
