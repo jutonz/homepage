3a7c5b21bb8575bd66f030cd021f6ff9111a9816
Justin Toniazzo <jutonz42@gmail.com>
2018-03-07 15:51:16 -0500

    Cleanup deploy scripts (#20)

--- docker/ci/testimage/deploy-service.sh
+++ docker/ci/testimage/deploy-service.sh
@@ -2,23 +2,16 @@
 
 set -e
 
-service=$1
-
-if [ -z "$service" ]; then
-  echo "usage: ./deploy-service.sh [SERVICE]"
-  exit 1
-fi
-
-echo "deploying $service"
+echo "deploying $SERVICE"
 
 echo $KUBELET_CONF | base64 -d > $KUBECONFIG
 
-if dctl k8s is-outdated $service -n homepage -q; then
+if dctl k8s is-outdated $SERVICE -n homepage; then
   cd docker/prod/k8s
-  kubectl set image deployments/$service `dctl tag-for $service` -nhomepage
-  kubectl rollout status deployments/$service -nhomepage
+  kubectl set image deployments/$SERVICE `dctl tag-for $SERVICE` -nhomepage
+  kubectl rollout status deployments/$SERVICE -nhomepage
 else
-  echo "$service would not be updated by deploy"
+  echo "$SERVICE would not be updated by deploy"
 fi
 
 rm -f $KUBECONFIG
