39661f7139be76cd981fcc275e82cccedcba70e1
Justin Toniazzo <jutonz42@gmail.com>
2018-03-07 15:47:33 -0500

    Finish refactor

--- docker/ci/testimage/push-service.sh
+++ docker/ci/testimage/push-service.sh
@@ -2,23 +2,16 @@
 
 set -e
 
-service=$1
-
-if [ -z "$service" ]; then
-  echo "usage: ./push-service.sh [SERVICE]"
-  exit 1
-fi
-
-echo "pushing $service"
+echo "pushing $SERVICE"
 
 echo $KUBELET_CONF | base64 -d > $KUBECONFIG
 
-if dctl k8s is-outdated $service -n homepage -q; then
+if dctl k8s is-outdated $SERVICE -n homepage -q; then
   docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-  dctl push $service
-  docker push `dctl tag-for $service --version=latest`
+  dctl push $SERVICE
+  docker push `dctl tag-for $SERVICE --version=latest`
 else
-  echo "$service would not be updated by push"
+  echo "$SERVICE would not be updated by push"
 fi
 
 rm -f $KUBECONFIG
