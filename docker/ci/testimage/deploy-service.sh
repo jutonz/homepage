3c0b4d4ec2ce979db043767f9e014a16a5c5f36f
Justin Toniazzo <jutonz42@gmail.com>
2018-03-07 20:42:09 -0500

    typo

--- docker/ci/testimage/deploy-service.sh
+++ docker/ci/testimage/deploy-service.sh
@@ -1,15 +1,15 @@
 #/bin/bash
 
 set +e
-set -x
 
 echo "deploying $SERVICE"
 
 echo $KUBELET_CONF | base64 -d > $KUBECONFIG
+set -x
 
 if dctl k8s is-outdated $SERVICE -n homepage; then
   cd docker/prod/k8s
-  kubectl set image deployments/$SERVICE `dctl tag-for $SERVICE` -nhomepage
+  kubectl set image deployments/$SERVICE app=`dctl tag-for $SERVICE` -nhomepage
   kubectl rollout status deployments/$SERVICE -nhomepage
 else
   echo "$SERVICE would not be updated by deploy"
