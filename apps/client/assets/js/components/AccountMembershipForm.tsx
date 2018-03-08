359b9c6058926e6999ea50b4dfc53735e7d7ce9a
Justin Toniazzo <jutonz42@gmail.com>
2018-03-08 08:39:47 -0500

    wip

---
+++ apps/client/assets/js/components/AccountMembershipForm.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class _AccountMembershipForm extends React.Component {
+  public render() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+export const AccountMembershipForm = _AccountMembershipForm;
--- apps/client/assets/js/routes/SettingsRoute.tsx
+++ apps/client/assets/js/routes/SettingsRoute.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import { MainNav, ActiveItem } from "./../components/MainNav";
 import { ChangePasswordForm } from "./../components/ChangePasswordForm";
 import { OneTimeLoginLink } from "./../components/OneTimeLoginLink";
+import { AccountMembershipForm } from "./../components/AccountMembershipForm";
 
 export const SettingsRoute = () => (
   <div>
     <MainNav activeItem={ActiveItem.Settings} />
     <ChangePasswordForm />
     <OneTimeLoginLink />
+    <AccountMembershipForm />
   </div>
 );
