893658e44fa3abdfcb83f843f7765cc424d9fd16
Justin Toniazzo <jutonz42@gmail.com>
2018-03-07 20:54:35 -0500

    Deploy from correct directory

--- docker/ci/testimage/deploy-service.sh
+++ docker/ci/testimage/deploy-service.sh
@@ -8,8 +8,8 @@ echo $KUBELET_CONF | base64 -d > $KUBECONFIG
 set -x
 
 if dctl k8s is-outdated $SERVICE -n homepage; then
-  cd docker/prod/k8s
-  kubectl set image deployments/$SERVICE app=`dctl tag-for $SERVICE` -nhomepage
+  tag=`dctl tag-for $SERVICE`
+  kubectl set image deployments/$SERVICE app=$tag -nhomepage
   kubectl rollout status deployments/$SERVICE -nhomepage
 else
   echo "$SERVICE would not be updated by deploy"
