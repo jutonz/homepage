10784e330727007ad961459740e8d17141595699
Justin Toniazzo <jutonz42@gmail.com>
2018-04-02 09:27:52 -0400

    back to cp

--- docker/dev/app/setup.sh
+++ docker/dev/app/setup.sh
@@ -18,14 +18,14 @@ rm -r deps
 
 # Copy hex deps which were installed during container build phase. See above
 # note about copying.
-mv /tmp/code/deps deps
+cp -r /tmp/code/deps deps
 
 # Remove build artifacts on host, if any
 rm -r _build
 
 # Copy hex deps which were compiled during container build phase. See above
 # note about copying.
-mv /tmp/code/_build _build
+cp -r /tmp/code/_build _build
 
 mix local.hex --force
 mix local.rebar --force
