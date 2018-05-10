8e693204c0b5fb7d28f4c8857212a7706d581090
Justin Toniazzo <jutonz42@gmail.com>
2018-05-09 20:54:42 -0400

    Format dates more nicely

--- apps/client/assets/js/components/ijust/IjustEventOccurrences.tsx
+++ apps/client/assets/js/components/ijust/IjustEventOccurrences.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
 import gql from "graphql-tag";
 import { Header, Table } from "semantic-ui-react";
+import { css, StyleSheet } from "aphrodite";
+import { format, distanceInWordsToNow } from "date-fns";
 
 import { QueryLoader } from "@utils/QueryLoader";
+import { Constants } from "@utils/Constants";
 
 const QUERY = gql`
   query GetIjustEventOccurrences($eventId: ID!) {
@@ -12,6 +15,12 @@ const QUERY = gql`
     }
   }
 `;
+
+const styles = StyleSheet.create({
+  relativeDateSpacer: {
+    marginLeft: "10px"
+  }
+});
 
 interface Props {
   eventId: string;
@@ -38,6 +47,11 @@ const renderOccurrences = occurrences => (
 
 const renderOccurrence = occurrence => (
   <Table.Row key={occurrence.id}>
-    <Table.Cell>{occurrence.insertedAt}</Table.Cell>
+    <Table.Cell>
+      {format(occurrence.insertedAt, Constants.dateTimeFormat)}
+      <span className={css(styles.relativeDateSpacer)}>
+        ({distanceInWordsToNow(occurrence.insertedAt)} ago)
+      </span>
+    </Table.Cell>
   </Table.Row>
 );
