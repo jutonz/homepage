965a5c25f24edcddd70a0c3c01ba8e5f066d5fa8
Justin Toniazzo <jutonz42@gmail.com>
2018-05-20 13:41:43 -0400

    touchup

--- apps/client/assets/js/components/ijust/IjustEventInput.tsx
+++ apps/client/assets/js/components/ijust/IjustEventInput.tsx
@@ -79,37 +79,3 @@ export class IjustEventInput extends React.Component<Props, State> {
     this.setState({ eventName });
   };
 }
-
-//const _IjustEventInput = ({
-//context,
-//name,
-//setName,
-//createEvent,
-//isCreating,
-//createErrors
-//}) => (
-//<div>
-//<Input value={name} onChange={(_ev, data) => setName(data.value)} />
-//{createErrors}
-//<Button
-//primary
-//onClick={() => createEvent(context.id, name)}
-//loading={isCreating}
-//>
-//Create
-//</Button>
-//</div>
-//);
-
-//export const IjustEventInput = connect(
-//(state: any) => ({
-//name: state.ijust.get("newEventName") || "",
-//isCreating: state.ijust.get("creatingEvent"),
-//createErrors: state.ijust.get("createErrors")
-//}),
-//dispatch => ({
-//setName: name => dispatch({ type: "SET_IJUST_NEW_EVENT_NAME", name }),
-//createEvent: (ijustContextId, name) =>
-//dispatch({ type: "IJUST_CREATE_EVENT", ijustContextId, name })
-//})
-//)(_IjustEventInput);
