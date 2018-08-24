95609751208dccaaa37149f830435ac7c03f6090
Justin Toniazzo <jutonz42@gmail.com>
2018-08-24 14:12:58 -0400

    Tests

--- apps/client/test/client_web/resolvers/ijust_resolver_test.exs
+++ apps/client/test/client_web/resolvers/ijust_resolver_test.exs
@@ -1,6 +1,6 @@
 defmodule ClientWeb.IjustResolverTest do
   use ClientWeb.ConnCase, async: true
-  alias Client.{TestUtils, Repo, Session, IjustContext, IjustEvent}
+  alias Client.{TestUtils, Session, IjustContext, IjustEvent}
 
   test "#search_events returns events", %{conn: conn} do
     conn = conn |> TestUtils.setup_current_user()
@@ -21,34 +21,30 @@ defmodule ClientWeb.IjustResolverTest do
 
     res = conn |> post("/graphql", %{query: query}) |> json_response(200)
 
-    require IEx; IEx.pry()
-
-
+    event_id_as_string = to_string(event.id)
+    %{"data" => %{"ijustEventsSearch" => [%{"id" => ^event_id_as_string}]}} = res
   end
 
-  test "#search_events does not return events if I cannot view the context" do
+  test "#search_events does not return events if I cannot view the context", %{conn: conn} do
+    conn = conn |> TestUtils.setup_current_user()
+    another_user = TestUtils.create_user(%{ email: "wee@mail.com", password: "password123"})
+    {:ok, not_my_context} = another_user.id |> IjustContext.get_default_context()
+    {:ok, _not_my_event} = another_user |> IjustEvent.add_for_user(%{
+      name: "hello",
+      ijust_context_id: not_my_context.id
+    })
+
+    query = """
+    query {
+      ijustEventsSearch(name: "hello", ijustContextId: #{not_my_context.id}) {
+        id
+      }
+    }
+    """
+
+    res = conn |> post("/graphql", %{query: query}) |> json_response(200)
+
+    %{"errors" => [%{"message" => error_message}]} = res
+    assert error_message == "No matching context"
   end
-
-  # describe "get_event_occurrences" do
-  # test "returns occurrences", %{conn: conn} do
-  # conn = conn |> TestUtils.setup_current_user()
-  # event_id = "123"
-
-  # {:ok, occurrence} =
-  # %IjustOccurrence{}
-  # |> IjustOccurrence.changeset(%{event_id: event_id})
-  # |> Repo.insert()
-
-  # query = """
-  # query {
-  # getIjustEventOccurrences(event_id: #{event_id}) { id }
-  # }
-  # """
-
-  # res = conn |> post("/graphql", %{query: query}) |> json_response(200)
-
-  # %{"data" => %{"getIjustEventOccurrences" => [%{"id" => id}]}} = res
-  # assert id == to_string(occurrence.id)
-  # end
-  # end
 end
