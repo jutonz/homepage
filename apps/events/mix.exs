a63aba1f8f538614750342ca592bdf406b345b16
Justin Toniazzo <jutonz42@gmail.com>
2018-09-22 08:25:12 -0400

    Add Events app (#135)

--- apps/events/mix.exs
+++ apps/events/mix.exs
@@ -1,16 +1,15 @@
-defmodule Twitch.MixProject do
+defmodule Events.MixProject do
   use Mix.Project
 
   def project do
     [
-      app: :twitch,
+      app: :events,
       version: "0.1.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
       elixir: "~> 1.6",
-      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -20,23 +19,14 @@ defmodule Twitch.MixProject do
   def application do
     [
       extra_applications: [:logger],
-      mod: {Twitch.Application, []},
+      mod: {Events.Application, []}
     ]
   end
-
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 1.3"},
-      {:gen_stage, "~> 0.14.0"},
-      {:websockex, "~> 0.4.1"},
-      {:exirc, "~> 1.0"},
-      {:ecto, "~> 2.2"},
-      {:postgrex, ">= 0.0.0"},
-      {:events, in_umbrella: true}
+      {:event_bus, "~> 1.5"}
     ]
   end
 end
--- apps/twitch/lib/twitch/producer.ex
+++ apps/twitch/lib/twitch/producer.ex
@@ -1,10 +1,5 @@
 defmodule Twitch.EventProducer do
   def publish(%Twitch.ParsedEvent{} = event) do
-    %EventBus.Model.Event{
-      id: EventBus.Util.Base62.unique_id(),
-      topic: :chat_message,
-      data: event
-    }
-    |> EventBus.notify()
+    Events.publish(event, :chat_message)
   end
 end
