3f1c77a56d590207c1bb9e37e0dbdcf0672efdf0
Justin Toniazzo <jutonz42@gmail.com>
2018-10-30 21:23:23 -0400

    this?

--- docker/prod/app/migrate.sh
+++ docker/prod/app/migrate.sh
@@ -3,9 +3,11 @@ set -e
 cd /root/code
 
 cd apps/client
-MIX_ENV=prod mix run ecto.create, ecto.migrate
+MIX_ENV=prod mix ecto.create
+MIX_ENV=prod mix ecto.migrate
 cd -
 
 cd apps/twitch
-MIX_ENV=prod mix run ecto.create, ecto.migrate
+MIX_ENV=prod mix ecto.create
+MIX_ENV=prod mix ecto.migrate
 cd -
