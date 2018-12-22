cac74288e6d0f30141189f4672b2b45f57bf523d
Justin Toniazzo <jutonz42@gmail.com>
2018-12-22 11:16:11 -0500

    also fix twitch db

--- apps/twitch/config/config.exs
+++ apps/twitch/config/config.exs
@@ -11,20 +11,13 @@ config :twitch,
     redirect_uri: System.get_env("TWITCH_REDIRECT_URI")
   }
 
-db_host = System.get_env("DB_HOST")
-db_port = System.get_env("DB_PORT")
-db_user = System.get_env("DB_USER")
-db_pass = System.get_env("DB_PASS")
-db_name = "homepage_twitch_" <> to_string(Mix.env())
-db_pool_size = System.get_env("DB_POOL_SIZE") || "10" |> String.to_integer()
+db_url = System.get_env("DATABASE_URL")
+db_pool_size = System.get_env("POOL_SIZE") || "10" |> String.to_integer()
 
 config :twitch, Twitch.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  hostname: db_host,
-  username: db_user,
-  password: db_pass,
-  port: db_port,
-  database: db_name,
-  pool_size: db_pool_size
+adapter: Ecto.Adapters.Postgres,
+  url: db_url,
+  pool_size: db_pool_size,
+  ssl: true
 
 import_config "#{Mix.env()}.exs"
