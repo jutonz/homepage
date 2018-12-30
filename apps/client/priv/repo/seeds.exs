09206903c6e62dc570e9fc9dc27c550c1a20e11f
Justin Toniazzo <jutonz42@gmail.com>
2018-12-30 11:33:49 -0500

    Add test user to seed file
    This it will make it easier to login to heroku branch deploys

--- apps/client/priv/repo/seeds.exs
+++ apps/client/priv/repo/seeds.exs
@@ -9,3 +9,16 @@
 #
 # We recommend using the bang functions (`insert!`, `update!` and so on) as
 # they will fail if something goes wrong.
+
+test_user_email = "test.user@example.com"
+test_user_password = System.get_env("TEST_USER_PASSWORD")
+
+case Client.User |> Client.Repo.get_by(email: test_user_email) do
+  nil -> %Client.User{}
+  user -> user
+end
+|> Client.User.changeset(%{
+  email: test_user_email,
+  password: test_user_password
+})
+|> Client.Repo.insert_or_update!()
