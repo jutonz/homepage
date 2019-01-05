d8964bef46f5f93d6837446ac5692d21b2d163eb
Justin Toniazzo <jutonz42@gmail.com>
2019-01-04 20:28:18 -0500

    start client app

--- apps/client/lib/mix/tasks/emoncms_digest.ex
+++ apps/client/lib/mix/tasks/emoncms_digest.ex
@@ -2,7 +2,7 @@ defmodule Mix.Tasks.EmoncmsDigest do
   use Mix.Task
 
   def run(_) do
-    {:ok, _started} = Application.ensure_all_started(:timex)
+    {:ok, _started} = Application.ensure_all_started(:client)
     Client.Email.emoncms_digest() |> Client.Mailer.deliver_later()
   end
 end
