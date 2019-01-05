df498b1b09088f41bba0658a167e6b3c288169cb
Justin Toniazzo <jutonz42@gmail.com>
2019-01-04 20:39:31 -0500

    deliver now

--- apps/client/lib/mix/tasks/emoncms_digest.ex
+++ apps/client/lib/mix/tasks/emoncms_digest.ex
@@ -3,6 +3,6 @@ defmodule Mix.Tasks.EmoncmsDigest do
 
   def run(_) do
     {:ok, _started} = Application.ensure_all_started(:client)
-    Client.Email.emoncms_digest() |> Client.Mailer.deliver_later()
+    Client.Email.emoncms_digest() |> Client.Mailer.deliver_now()
   end
 end
