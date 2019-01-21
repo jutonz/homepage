a6bfd7749fbb1d198942a57d69044b31be5642e9
Justin Toniazzo <jutonz42@gmail.com>
2019-01-21 12:03:33 -0500

    fix twitch config

--- apps/twitch/config/test.exs
+++ apps/twitch/config/test.exs
@@ -4,4 +4,6 @@ config :twitch, Twitch.Repo,
   adapter: Ecto.Adapters.Postgres,
   pool: Ecto.Adapters.SQL.Sandbox,
   database: "homepage_twitch_test",
-  hostname: "localhost"
+  hostname: "localhost",
+  username: "postgres",
+  password: nil
