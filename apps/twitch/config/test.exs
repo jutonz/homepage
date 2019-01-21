0cb1e252ab8d2c72466dbfc80132771f955464cd
Justin Toniazzo <jutonz42@gmail.com>
2019-01-21 11:31:22 -0500

    Fix db config issues in local test env

--- apps/twitch/config/test.exs
+++ apps/twitch/config/test.exs
@@ -4,8 +4,4 @@ config :twitch, Twitch.Repo,
   adapter: Ecto.Adapters.Postgres,
   pool: Ecto.Adapters.SQL.Sandbox,
   database: "homepage_twitch_test",
-  hostname: "localhost",
-  username: "homepage",
-  password: nil,
-  port: 5432,
-  pool_size: 10
+  hostname: "localhost"
