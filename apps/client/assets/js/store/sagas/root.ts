83df3b4eabf4206199bd530475c2197673623712
Justin Toniazzo <jutonz42@gmail.com>
2019-02-02 09:05:53 -0500

    Remove ijust saga
    It was replaced with apollo a while ago but never actually removed.

--- apps/client/assets/js/store/sagas/root.ts
+++ apps/client/assets/js/store/sagas/root.ts
@@ -1,10 +1,9 @@
 import { all } from "redux-saga/effects";
 
 import flash from "./flash";
-import ijust from "./ijust";
 import teams from "./teams";
 import users from "./users";
 
 export function* rootSaga() {
-  yield all([teams(), users(), flash(), ijust()]);
+  yield all([teams(), users(), flash()]);
 }
