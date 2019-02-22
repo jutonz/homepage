210747905bbe8b7b922d158d0a62cab22ce90862
Justin Toniazzo <jutonz42@gmail.com>
2019-02-22 11:43:05 -0500

    Add twitch database

--- apps/twitch/config/prod.exs
+++ apps/twitch/config/prod.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-db_url = System.get_env("DATABASE_URL")
+db_url = System.get_env("HEROKU_POSTGRESQL_PINK_URL")
 db_pool_size = System.get_env("POOL_SIZE") || "10" |> String.to_integer()
 
 config :twitch, Twitch.Repo,
