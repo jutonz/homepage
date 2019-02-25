1d9d766543f5fd77013fc0db4085221d63fd11c2
Justin Toniazzo <jutonz42@gmail.com>
2019-02-25 14:01:17 -0500

    Manually specify db TLS certifciates
    Trying out using a different hosting provider than heroku for twitch db
    stuff since we're storing quite a lot of data daily.

--- apps/twitch/config/prod.exs
+++ apps/twitch/config/prod.exs
@@ -3,8 +3,20 @@ use Mix.Config
 db_url = System.get_env("TWITCH_DATABASE_URL")
 db_pool_size = System.get_env("POOL_SIZE") || "10" |> String.to_integer()
 
+ssl_opts =
+  if System.get_env("TWITCH_DB_SERVER_CA") do
+    [
+      cacertfile: "priv/server-ca.pem",
+      keyfile: "priv/client-key.pem",
+      certfile: "priv/client-cert.pem"
+    ]
+  else
+    []
+  end
+
 config :twitch, Twitch.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: db_url,
   pool_size: db_pool_size,
-  ssl: true
+  ssl: true,
+  ssl_opts: ssl_opts
