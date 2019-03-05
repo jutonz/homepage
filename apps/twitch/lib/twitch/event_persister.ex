e357f016ba8f7e981d6cc77be1b568ba36713d7d
Justin Toniazzo <jutonz42@gmail.com>
2019-03-05 11:27:39 -0500

    Allow skipping chat event persistence
    Not wired up to anything currently, but eventually can be

--- apps/twitch/lib/twitch/event_persister.ex
+++ apps/twitch/lib/twitch/event_persister.ex
@@ -20,31 +20,43 @@ defmodule Twitch.EventPersister do
   end
 
   # Ignore these commands
-  def make_state(%{irc_command: "MODE"}, state), do: state
-  def make_state(%{irc_command: "GLOBALUSERSTATE"}, state), do: state
-  def make_state(%{irc_command: "CAP"}, state), do: state
-  def make_state(%{irc_command: "353"}, state), do: state
+  def update_state(%{irc_command: "MODE"}, state), do: state
+  def update_state(%{irc_command: "GLOBALUSERSTATE"}, state), do: state
+  def update_state(%{irc_command: "CAP"}, state), do: state
+  def update_state(%{irc_command: "353"}, state), do: state
 
   # Create changest and add to state
-  def make_state(event_struct, state) do
-    cset = %Twitch.TwitchEvent{} |> Twitch.TwitchEvent.changeset(event_struct)
-    events = Enum.concat(state, [cset])
+  def update_state(event_struct, state) do
+    events = add_event_struct_to_state(event_struct, state)
 
     if length(events) >= @persist_after do
-      events
-      |> Enum.each(fn event ->
-        case event |> Twitch.Repo.insert() do
-          {:ok, ev} ->
-            Events.publish(ev, :twitch_event_created)
-
-          _ ->
-            nil
-        end
-      end)
-
+      events |> Enum.each(&persist_event/1)
       []
     else
       events
+    end
+  end
+
+  def add_event_struct_to_state(event_struct, state) do
+    if should_persist_event?(event_struct) do
+      cset = %Twitch.TwitchEvent{} |> Twitch.TwitchEvent.changeset(event_struct)
+      Enum.concat(state, [cset])
+    else
+      state
+    end
+  end
+
+  def should_persist_event?(event_struct) do
+    true
+  end
+
+  def persist_event(event) do
+    case event |> Twitch.Repo.insert() do
+      {:ok, ev} ->
+        Events.publish(ev, :twitch_event_created)
+
+      _ ->
+        nil
     end
   end
 
@@ -52,7 +64,7 @@ defmodule Twitch.EventPersister do
     event = Events.fetch_event(event_shadow)
 
     struct = event.data |> Map.from_struct()
-    new_state = make_state(struct, state)
+    new_state = update_state(struct, state)
 
     Events.mark_as_completed({__MODULE__, event_shadow})
 
