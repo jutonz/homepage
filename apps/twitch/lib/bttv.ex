dd0b7f02af5ec85dd824b9de5da6eba54b11db95
Justin Toniazzo <jutonz42@gmail.com>
2019-03-09 16:07:23 -0500

    Add emote detectors
    Detect emotes. Currently just bttv but can maybe add twitch ones soon

---
+++ apps/twitch/lib/bttv.ex
@@ -0,0 +1,9 @@
+defmodule Twitch.Bttv do
+  alias Twitch.Bttv
+
+  def channel_emotes(channel_name) do
+    Bttv.Api.connection(:get, "channels/#{channel_name}")
+    |> Map.get("emotes")
+    |> Enum.map(&Bttv.Emote.from_bttv_json/1)
+  end
+end
