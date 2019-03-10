63dd1e2ff18fe3c7246a7e95c512f3458bafcfde
Justin Toniazzo <jutonz42@gmail.com>
2019-03-10 11:32:04 -0400

    bttv global emotes

--- apps/twitch/lib/bttv.ex
+++ apps/twitch/lib/bttv.ex
@@ -12,4 +12,16 @@ defmodule Twitch.Bttv do
     |> Map.get("emotes")
     |> Enum.map(&Bttv.Emote.from_bttv_json/1)
   end
+
+  def global_ffz_emotes do
+    Bttv.Api.connection(:get, "frankerfacez_emotes/global")
+    |> Map.get("emotes")
+    |> Enum.map(&Bttv.Emote.from_bttv_json/1)
+  end
+
+  def channel_ffz_emotes(channel_id) do
+    Bttv.Api.connection(:get, "frankerfacez_emotes/channels/#{channel_id}")
+    |> Map.get("emotes")
+    |> Enum.map(&Bttv.Emote.from_bttv_json/1)
+  end
 end
