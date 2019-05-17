57d36f6d560b420379beb336cd37fb3b247c849a
Justin Toniazzo <jutonz42@gmail.com>
2019-05-17 14:19:43 -0400

    Rename twitch db env var (#519)
    Make it easier to disginguish down the line.

--- apps/twitch/config/prod.exs
+++ apps/twitch/config/prod.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
-db_url = System.get_env("TWITCH_DATABASE_URL")
+db_google_url = System.get_env("TWITCH_GOOGLE_DATABASE_URL")
 db_pool_size = System.get_env("POOL_SIZE") || "10" |> String.to_integer()
 
 ssl_opts =
@@ -16,7 +16,7 @@ ssl_opts =
 
 config :twitch, Twitch.GoogleRepo,
   adapter: Ecto.Adapters.Postgres,
-  url: db_url,
+  url: db_google_url,
   pool_size: db_pool_size,
   ssl: true,
   ssl_opts: ssl_opts
