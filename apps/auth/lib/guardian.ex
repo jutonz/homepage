8bd23e2eb3929ee4f8cc05302d86d0471849ed8b
Justin Toniazzo <jutonz42@gmail.com>
2019-07-06 10:08:53 -0400

    Fix guardian compile warning

--- apps/auth/lib/guardian.ex
+++ apps/auth/lib/guardian.ex
@@ -1,11 +1,9 @@
 defmodule Auth.Guardian do
   use Guardian, otp_app: :auth
-  alias Client.{User, Repo}
 
   def subject_for_token(resource_id, _claims), do: {:ok, resource_id}
 
   def resource_from_claims(%{"sub" => id}) do
-    # assumes User resource (problematic?)
-    {:ok, Repo.get(User, id)}
+    {:ok, id}
   end
 end
