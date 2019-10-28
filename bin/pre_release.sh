7fb2f22c58a6867f0b208f4f44aad6661907b3ab
Justin Toniazzo <jutonz42@gmail.com>
2019-10-28 19:48:03 -0400

    add CONSOLE to migrate commands

--- bin/pre_release.sh
+++ bin/pre_release.sh
@@ -6,9 +6,9 @@ cd apps/twitch
 echo $TWITCH_DB_CLIENT_CERT | base64 --decode > priv/client-cert.pem
 echo $TWITCH_DB_CLIENT_KEY | base64 --decode > priv/client-key.pem
 echo $TWITCH_DB_SERVER_CA | base64 --decode > priv/server-ca.pem
-mix ecto.migrate -r Twitch.Repo
+CONSOLE=yes mix ecto.migrate -r Twitch.Repo
 cd -
 
 cd apps/client
-mix do ecto.migrate, run priv/repo/seeds.exs
+CONSOLE=yes mix do ecto.migrate, run priv/repo/seeds.exs
 cd -
