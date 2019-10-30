2db097acfc0c13a3c909eae852fecc24fbfbe1be
Justin Toniazzo <jutonz42@gmail.com>
2019-10-30 16:48:26 -0400

    Correctly use hmac hash
    We were calculating the hash wrong

--- apps/twitch/lib/twitch/webhook_subscriptions/signing.ex
+++ apps/twitch/lib/twitch/webhook_subscriptions/signing.ex
@@ -2,13 +2,13 @@ defmodule Twitch.WebhookSubscriptions.Signing do
   def signature(body) do
     signature =
       :sha256
-      |> :crypto.hash([signing_secret(), body])
+      |> :crypto.hmac(secret(), body)
       |> Base.encode16()
 
     "sha256=" <> signature
   end
 
-  defp signing_secret do
+  defp secret do
     Application.get_env(:twitch, :webhook_secret)
   end
 end
