c54a595808763cbb524caae4acd861ef4898c579
Justin Toniazzo <jutonz42@gmail.com>
2020-01-29 20:54:57 -0500

    Improve water log styling
    Use super advanced features like tables

--- apps/client/lib/client_web/views/water_log_view.ex
+++ apps/client/lib/client_web/views/water_log_view.ex
@@ -1,3 +1,12 @@
 defmodule ClientWeb.WaterLogView do
   use ClientWeb, :view
+  alias Timex.Format.DateTime.Formatters.Strftime
+  alias Timex.Timezone
+
+  @format "%d %b %Y %H:%M"
+  def formatted_date(date),
+    do: date |> Timezone.convert(timezone()) |> Strftime.format!(@format)
+
+  defp timezone,
+    do: Application.get_env(:client, :default_timezone)
 end
