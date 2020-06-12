13f5cb46ec249a4d5daac7edb363281de538c1c7
Justin Toniazzo <jutonz42@gmail.com>
2020-06-11 20:54:44 -0400

    Update phoenix_live_view to 0.7 (#1145)

--- apps/client/lib/client_web.ex
+++ apps/client/lib/client_web.ex
@@ -52,8 +52,6 @@ defmodule ClientWeb do
         only: [
           live_render: 2,
           live_render: 3,
-          live_link: 1,
-          live_link: 2,
           live_component: 2,
           live_component: 3,
           live_component: 4
