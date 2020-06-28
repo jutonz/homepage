f14a1d656dd2b8d7fa1025fd762aec1135af91cd
Justin Toniazzo <jutonz42@gmail.com>
2020-06-28 13:00:05 -0400

    Upgrade from ecto to ecto_sql
    This split happened a long time ago, so I'm pretty late to the party
    here. I wonder why dependabot didn't pick it up? 🤔

--- apps/client/mix.exs
+++ apps/client/mix.exs
@@ -39,8 +39,8 @@ defmodule Client.Mixfile do
     [
       {:phoenix, "1.4.17"},
       {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.2"},
-      {:postgrex, ">= 0.0.0"},
+      {:phoenix_ecto, "~> 4.0"},
+      {:postgrex, "~> 0.14"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
