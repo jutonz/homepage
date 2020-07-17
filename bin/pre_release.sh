e982c3027ddc98d59f53bb9f487a22988bfac9d0
Justin Toniazzo <jutonz42@gmail.com>
2020-07-16 21:16:11 -0400

    Increase db pool size during release command (#1209)
    While running the heroku release command, use up to 5 db connections per
    app. We were seeing some timeouts, so hopefully a larger pool will allow
    for more concurrent requests.

--- bin/pre_release.sh
+++ bin/pre_release.sh
@@ -6,9 +6,9 @@ cd apps/twitch
 echo $TWITCH_DB_CLIENT_CERT | base64 --decode > priv/client-cert.pem
 echo $TWITCH_DB_CLIENT_KEY | base64 --decode > priv/client-key.pem
 echo $TWITCH_DB_SERVER_CA | base64 --decode > priv/server-ca.pem
-POOL_SIZE=2 CONSOLE=yes mix ecto.migrate -r Twitch.Repo
+POOL_SIZE=5 CONSOLE=yes mix ecto.migrate -r Twitch.Repo
 cd -
 
 cd apps/client
-POOL_SIZE=2 CONSOLE=yes mix do ecto.migrate, run priv/repo/seeds.exs
+POOL_SIZE=5 CONSOLE=yes mix do ecto.migrate, run priv/repo/seeds.exs
 cd -
