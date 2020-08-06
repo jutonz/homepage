0d98acbeeec3cb1dd67748cdcc7df57ddad7905e
Justin Toniazzo <jutonz42@gmail.com>
2020-08-06 17:06:50 -0400

    handle incomming events

--- apps/client/lib/client_web/channels/water_log_channel.ex
+++ apps/client/lib/client_web/channels/water_log_channel.ex
@@ -7,7 +7,31 @@ defmodule ClientWeb.WaterLogChannel do
     user_id = socket.assigns[:user_id]
     case WaterLogs.get_by_user_id(user_id, water_log_id) do
       nil -> {:error, %{"reason" => "no such water log"}}
-      _log -> {:ok, socket}
+      _log -> {:ok, assign(socket, :water_log_id, water_log_id)}
+    end
+  end
+
+  def handle_in("set_ml", %{"ml" => ml}, socket) do
+    Logger.info("Setting ml to #{ml}")
+    {:noreply, assign(socket, :ml, ml)}
+  end
+
+  def handle_in("commit", %{"ml" => ml}, socket) do
+    entry_params = %{
+      ml: ml,
+      user_id: socket.assigns[:user_id],
+      water_log_id: socket.assigns[:water_log_id]
+    }
+
+    case WaterLogs.create_entry(entry_params) |> IO.inspect() do
+      {:ok, _entry} ->
+        {:noreply, assign(socket, :ml, 0)}
+
+      {:error, changeset} ->
+        error = %{
+          "error" => Client.Util.errors_to_sentence(changeset)
+        }
+        {:reply, {:error, error}, socket}
     end
   end
 end
