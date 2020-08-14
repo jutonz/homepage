a543f938208ae376ca6909f399a6f2f91e607ec4
Justin Toniazzo <jutonz42@gmail.com>
2020-08-14 08:48:12 -0400

    Automatically update total dispensed (#1253)
    When a new water log entry is created via a websocket, also update the
    total amount dispensed.
    
    Also add some more testsing around the kiosk, since before there was
    none. Testing liveviews is actually super easy--nice! Not having to deal
    with jest is a huuuuuge win

--- apps/client/lib/client_web/channels/water_log_channel.ex
+++ apps/client/lib/client_web/channels/water_log_channel.ex
@@ -12,38 +12,24 @@ defmodule ClientWeb.WaterLogChannel do
     end
   end
 
-  def handle_in("set_ml", %{"ml" => ml}, socket) do
-    Logger.info("Setting ml to #{ml} #{inspect(socket.assigns)}")
-
-    Phoenix.PubSub.broadcast!(
-      Client.PubSub,
-      "water_log_internal:#{socket.assigns[:water_log_id]}",
-      {:set_ml, %{"ml" => ml}}
-    )
-
+  def handle_in("set_ml", %{"ml" => ml}, %{assigns: assigns} = socket) do
+    Logger.info("Setting ml to #{ml} #{inspect(assigns)}")
+    publish_event(assigns[:water_log_id], {:set_ml, %{"ml" => ml}})
     {:noreply, assign(socket, :ml, ml)}
   end
 
-  def handle_in("commit", %{"ml" => ml}, socket) do
+  def handle_in("commit", %{"ml" => ml}, %{assigns: assigns} = socket) do
     entry_params = %{
       ml: ml,
-      user_id: socket.assigns[:user_id],
-      water_log_id: socket.assigns[:water_log_id]
+      user_id: assigns[:user_id],
+      water_log_id: assigns[:water_log_id]
     }
 
-    Phoenix.PubSub.broadcast!(
-      Client.PubSub,
-      "water_log_internal:#{socket.assigns[:water_log_id]}",
-      {:saving, %{"ml" => ml}}
-    )
+    publish_event(assigns[:water_log_id], {:saving, %{"ml" => ml}})
 
     case WaterLogs.create_entry(entry_params) |> IO.inspect() do
       {:ok, _entry} ->
-        Phoenix.PubSub.broadcast!(
-          Client.PubSub,
-          "water_log_internal:#{socket.assigns[:water_log_id]}",
-          :saved
-        )
+        publish_event(assigns[:water_log_id], :saved)
 
         {:noreply, assign(socket, :ml, 0)}
 
@@ -54,5 +40,13 @@ defmodule ClientWeb.WaterLogChannel do
 
         {:reply, {:error, error}, socket}
     end
+  end
+
+  defp publish_event(log_id, event) do
+    Phoenix.PubSub.broadcast!(
+      Client.PubSub,
+      "water_log_internal:#{log_id}",
+      event
+    )
   end
 end
