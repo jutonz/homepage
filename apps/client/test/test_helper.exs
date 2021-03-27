fb8a2c59183c2dfeb2b294d5e4efd98be8a1743d
Justin Toniazzo <jutonz42@gmail.com>
2021-03-27 10:14:21 -0400

    cleanup

--- apps/client/test/test_helper.exs
+++ apps/client/test/test_helper.exs
@@ -4,5 +4,3 @@ Application.put_env(:wallaby, :base_url, ClientWeb.Endpoint.url)
 
 {:ok, _} = Application.ensure_all_started(:ex_machina)
 {:ok, _} = Application.ensure_all_started(:wallaby)
-
-#Ecto.Adapters.SQL.Sandbox.mode(Client.Repo, :manual)
