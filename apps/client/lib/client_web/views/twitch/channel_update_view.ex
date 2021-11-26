3f09e72aea9d13146686641bf4fb1fe7a12440b0
Justin Toniazzo <jutonz42@gmail.com>
2021-11-25 21:01:56 -0500

    Fix translations for channel update types

--- apps/client/lib/client_web/views/twitch/channel_update_view.ex
+++ apps/client/lib/client_web/views/twitch/channel_update_view.ex
@@ -2,4 +2,7 @@ defmodule ClientWeb.Twitch.ChannelUpdateView do
   use ClientWeb, :view
 
   def type("channel.update"), do: "Update"
+  def type("stream.online"), do: "Online"
+  def type("stream.offline"), do: "Offline"
+  def type(type), do: type
 end
