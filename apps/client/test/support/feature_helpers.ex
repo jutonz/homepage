feca3338112ffbf1d140d7019bf0c0e6bf218f2c
Justin Toniazzo <jutonz@hey.com>
2022-08-20 10:48:38 -0400

    Add login test (#2246)
    Our first react feature test. yay!

--- apps/client/test/support/feature_helpers.ex
+++ apps/client/test/support/feature_helpers.ex
@@ -1,10 +1,25 @@
 defmodule ClientWeb.FeatureHelpers do
   use Wallaby.DSL
-  import Wallaby.Query, only: [css: 2]
+
+  import Wallaby.Query,
+    only: [
+      button: 1,
+      css: 2,
+      fillable_field: 1
+    ]
 
   def role(role, opts \\ []),
     do: role |> role_selector() |> css(opts)
 
   defp role_selector(role),
     do: "[data-role=\"#{role}\"]"
+
+  def login(session, user) do
+    session
+    |> visit("/#/login")
+    |> fill_in(fillable_field("email"), with: user.email)
+    |> fill_in(fillable_field("password"), with: user.password)
+    |> click(button("Login"))
+    |> find(css("a", text: "Logout"))
+  end
 end
