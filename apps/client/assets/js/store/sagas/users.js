8e83819fdb9596b13f0d2a39690017faacac22ff
Justin Toniazzo <jutonz@hey.com>
2023-03-18 13:22:51 -0400

    mui team leave and delete forms (#2438)
    also tests!
    
    this is the last use of the team saga?

--- apps/client/assets/js/store/sagas/users.js
+++ apps/client/assets/js/store/sagas/users.js
@@ -1,5 +1,5 @@
 import { put, takeEvery } from "redux-saga/effects";
-import { fetchUserQuery, fetchTeamUserQuery } from "./../queries";
+import { fetchUserQuery } from "./../queries";
 import collectGraphqlErrors from "./../../utils/collectGraphqlErrors";
 
 function* fetchUser({ id }) {
@@ -15,19 +15,6 @@ function* fetchUser({ id }) {
   }
 }
 
-function* fetchTeamUser({ teamId, userId }) {
-  try {
-    yield put({ type: "FETCH_TEAM_USER_REQUEST", userId });
-    const user = yield fetchTeamUserQuery({ teamId, userId });
-    yield put({ type: "STORE_USER", user });
-    yield put({ type: "FETCH_TEAM_USER_SUCCESS", userId });
-  } catch (error) {
-    console.error(error);
-    const errors = collectGraphqlErrors(error);
-    yield put({ type: "FETCH_TEAM_USER_FAILURE", userId, errors });
-  }
-}
-
 function* storeUser({ user }) {
   yield put({ type: "STORE_USERS", users: [user] });
 }
@@ -35,5 +22,4 @@ function* storeUser({ user }) {
 export default function* () {
   yield takeEvery("FETCH_USER", fetchUser);
   yield takeEvery("STORE_USER", storeUser);
-  yield takeEvery("FETCH_TEAM_USER", fetchTeamUser);
 }
