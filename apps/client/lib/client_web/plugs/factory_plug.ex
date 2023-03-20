c1ce0556ef69ca99119e60219310360fdc39abc2
Justin Toniazzo <jutonz@hey.com>
2023-03-20 12:23:08 -0400

    Fix loading issue with factory plug
    Maybe because the dependncy isn't explicit as the module is passed in,
    but sometimes calls to Client.Factory failed, even though I know for
    sure that it's complied and available. Adding `Code.ensure_loaded`
    seemed to fix that. Is this the right thing to do?

--- apps/client/lib/client_web/plugs/factory_plug.ex
+++ apps/client/lib/client_web/plugs/factory_plug.ex
@@ -12,36 +12,34 @@ defmodule ClientWeb.Plugs.FactoryPlug do
 
   def call(%Conn{method: "POST"} = conn, opts) do
     if matches_request_path?(conn, opts) do
+      factory_module = get_factory_module(opts)
       factory_name = String.replace(conn.request_path, opts[:at] <> "/", "")
       factory_function = build_factory_function(factory_name)
 
-      result =
-        cond do
-          Kernel.function_exported?(opts[:factory], factory_function, 1) ->
-            attrs =
-              conn.body_params
-              |> Enum.to_list()
-              |> Enum.map(fn {key, value} -> {String.to_atom(key), value} end)
-              |> Map.new()
+      with {:ok, result} <- call_factory_function(factory_module, factory_function, conn) do
+        case opts[:repo].insert(result) do
+          {:ok, record} ->
+            conn
+            |> put_resp_header("content-type", "application/json")
+            |> send_resp(201, Jason.encode!(record))
+            |> halt()
 
-            apply(opts[:factory], factory_function, [attrs])
+          {:error, changeset} ->
+            IO.inspect(changeset)
 
-          Kernel.function_exported?(opts[:factory], factory_function, 0) ->
-            apply(opts[:factory], factory_function, [])
+            conn
+            |> send_resp(400, "something went wrong")
         end
-
-      case opts[:repo].insert(result) do
-        {:ok, record} ->
+      else
+        {:error, reason} ->
           conn
-          |> put_resp_header("content-type", "application/json")
-          |> send_resp(201, Jason.encode!(record))
+          |> send_resp(500, reason)
           |> halt()
 
-        {:error, changeset} ->
-          IO.inspect(changeset)
-
+        _ ->
           conn
-          |> send_resp(400, "something went wrong")
+          |> send_resp(500, "An unknown error occurred :(")
+          |> halt()
       end
     else
       conn
@@ -56,6 +54,36 @@ defmodule ClientWeb.Plugs.FactoryPlug do
     record
     |> Kernel.<>("_factory")
     |> String.to_atom()
+  end
+
+  defp get_factory_module(opts) do
+    module = Map.fetch!(opts, :factory)
+
+    if !Code.ensure_loaded?(module) do
+      raise "Couldn't find factory module #{module}. Did you configure #{__MODULE__} correctly?"
+    end
+
+    module
+  end
+
+  defp call_factory_function(factory_module, factory_function, conn) do
+    cond do
+      Kernel.function_exported?(factory_module, factory_function, 1) ->
+        attrs =
+          conn.body_params
+          |> Enum.to_list()
+          |> Enum.map(fn {key, value} -> {String.to_atom(key), value} end)
+          |> Map.new()
+
+        {:ok, apply(factory_module, factory_function, [attrs])}
+
+      Kernel.function_exported?(factory_module, factory_function, 0) ->
+        {:ok, apply(factory_module, factory_function, [])}
+
+      true ->
+        {:error,
+         "Couldn't find matching factory. Expected #{factory_module} to define a function #{factory_function}/0 or #{factory_function}/1, but no such functions were found."}
+    end
   end
 
   defp matches_request_path?(%Conn{request_path: path}, %{at: at}) do
