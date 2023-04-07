4eac80bfe1429dc6da95757b75c06c2a8eaa28a2
Justin Toniazzo <jutonz@hey.com>
2023-04-07 12:05:46 -0400

    Output metafile from esbuild for bundle analysis
    When buiding the prod bundle, output a metafile which can be entered
    into the [bundle analyzer](https://esbuild.github.io/analyze/)

--- apps/client/assets/scripts/compile_static.mjs
+++ apps/client/assets/scripts/compile_static.mjs
@@ -1,6 +1,9 @@
 #!/usr/bin/env node
 
 import { build, context } from "esbuild";
+import { writeFileSync } from "fs";
+import { resolve } from "path";
+
 import { createStaticDir } from "./create-static-dir.mjs";
 
 const getEnv = () => {
@@ -28,17 +31,27 @@ const esbuildOpts = {
   bundle: true,
   sourcemap: true,
   minify: isProd,
+  metafile: isProd,
   outdir: "./../priv/static/",
   target: ["chrome90", "firefox90", "safari14"],
   logLevel: "debug",
 };
+
+function writeMetafile(metaObject) {
+  if (!metaObject) return;
+
+  const metaFileLocaton = resolve("../../../esbuild-meta.json");
+  writeFileSync(metaFileLocaton, JSON.stringify(metaObject));
+  console.log("Wrote meta file to: ", metaFileLocaton);
+}
 
 (async function compile() {
   try {
     await createStaticDir();
 
     if (isProd) {
-      await build(esbuildOpts);
+      const result = await build(esbuildOpts);
+      writeMetafile(result.metafile);
     } else {
       const mainContext = await context(esbuildOpts);
       await mainContext.watch();
