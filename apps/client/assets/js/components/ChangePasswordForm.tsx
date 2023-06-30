b10c7401affbf65575f263f414685d32ad144d95
Justin Toniazzo <jutonz@hey.com>
2023-06-30 10:10:19 -0400

    fix tsc issue?

--- apps/client/assets/js/components/ChangePasswordForm.tsx
+++ apps/client/assets/js/components/ChangePasswordForm.tsx
@@ -3,7 +3,7 @@ import Alert from "@mui/material/Alert";
 import Button from "@mui/material/Button";
 import { enqueueSnackbar } from "notistack";
 import React, { useCallback } from "react";
-import { useForm } from "react-hook-form";
+import { useForm, FieldError } from "react-hook-form";
 import { useMutation } from "urql";
 import * as yup from "yup";
 
@@ -26,7 +26,7 @@ interface FormInputs {
   currentPassword: string;
   newPassword: string;
   newPasswordConfirm: string;
-  backendError: null;
+  backendError: null | FieldError;
 }
 
 const schema = yup
