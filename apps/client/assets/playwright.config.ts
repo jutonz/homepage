dea4e46ed8abba2f9306329588de79dd98e09f98
Justin Toniazzo <jt@thoughtbot.com>
2024-05-12 10:43:25 -0400

    try manually starting server

--- apps/client/assets/playwright.config.ts
+++ apps/client/assets/playwright.config.ts
@@ -70,9 +70,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'yarn test:server',
-    url: 'http://localhost:4002',
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: 'yarn test:server',
+  //   url: 'http://localhost:4002',
+  //   reuseExistingServer: !process.env.CI
+  // },
 });
