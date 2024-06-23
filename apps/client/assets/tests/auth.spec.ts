5c163e57698dc9342308328c7c63a94169066220
Justin Toniazzo <jutonz@hey.com>
2024-06-23 11:42:54 -0400

    Add playwright (#3063)
    Start the process of migrating from cypress to playwright, which has more normal JS syntax and is generally a lot closer to how you'd write jest specs.

---
+++ apps/client/assets/tests/auth.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from "@playwright/test";
+
+const BASE_URL = "http://localhost:4002";
+
+test.describe("Auth", () => {
+  test("allows me to sign up", async ({ page }) => {
+    await page.goto(BASE_URL + "/#/login?bg=false");
+    await page.getByRole("link", { name: "Or signup" }).click();
+
+    const email = `wee-${Math.random()}@example.com`;
+    await page.getByRole("textbox", { name: "Email" }).fill(email);
+    await page.getByRole("textbox", { name: "Password" }).fill("password123");
+    await page.getByRole("button", { name: "Signup" }).click();
+
+    await expect(page.getByRole("link", { name: "Logout" })).toBeVisible();
+  });
+});
