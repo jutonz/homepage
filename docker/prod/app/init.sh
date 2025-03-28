5b0232832e651e3b6ed0e085c710679fb02079d6
Justin Toniazzo <jutonz@hey.com>
2025-03-28 08:21:06 -0400

    Remove k8s and multi-env dockerfiles (#3589)
    When I originally created this app in 2018 I was very interested in
    docker, especially for local development. I had been working on an app
    which required a bit of setup and my team used vagrant, so I was
    interested in exploring alternatives.
    
    Since then I've gone back to preferring just running everything locally.
    Though I will admit running in docker is much more seamless than it used
    to be.
    
    Anyway I had a bunch of docker configs for running the app locally as
    well as in prod. This includes k8s configs for prod. I'm not using
    those, and while I have some interest in moving back from kamal to k8s
    (or really k3s), I think I need to start from scratch rather than
    reusing all these old configs. So here I delete them.

--- docker/prod/app/init.sh
+++
@@ -1,3 +0,0 @@
-cd /root/code
-
-PORT=3000 MIX_ENV=prod mix phx.server
--- docker/prod/nginx/init.sh
+++
@@ -1,8 +0,0 @@
-set -x
-set -e
-
-touch /var/log/nginx/homepage.access.log
-
-service nginx start
-
-tail -f /var/log/nginx/homepage.error.log
