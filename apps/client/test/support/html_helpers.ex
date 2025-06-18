8cb8cefc801fcc63cfc8163df84c0496dd036673
Justin Toniazzo <jt@thoughtbot.com>
2025-06-18 11:05:31 -0400

    fix deprecations
    can't pass a string into Floki.find, need to parse html doc first

--- apps/client/test/support/html_helpers.ex
+++ apps/client/test/support/html_helpers.ex
@@ -2,6 +2,15 @@ defmodule ClientWeb.HtmlHelpers do
   import ExUnit.Assertions, only: [assert: 1]
 
   def assert_contains_selector(parsed_html, css_selector) do
+    parsed_html =
+      case parsed_html do
+        string when is_binary(string) ->
+          Floki.parse_document!(parsed_html)
+
+        _ ->
+          parsed_html
+      end
+
     ele = Floki.find(parsed_html, css_selector)
 
     assert [ele] = ele
