96c1800d553fc97c6ed051671b6aad49cc62e2a1
Justin Toniazzo <jutonz@hey.com>
2025-07-02 09:06:46 -0400

    Fix sql sandbox usage (#3758)
    ecto_sql updated the signature to have a 4th argument. I need to also
    capture that to pass it through.

--- apps/client/lib/client_web/plugs/sandbox.ex
+++ apps/client/lib/client_web/plugs/sandbox.ex
@@ -2,7 +2,7 @@ defmodule ClientWeb.Plugs.Sandbox do
   defdelegate checkin(repo, args), to: Ecto.Adapters.SQL.Sandbox
   defdelegate checkout(repo, args), to: Ecto.Adapters.SQL.Sandbox
 
-  def allow(repo, owner_pid, child_pid) do
-    Ecto.Adapters.SQL.Sandbox.allow(repo, owner_pid, child_pid)
+  def allow(repo, owner_pid, child_pid, opts) do
+    Ecto.Adapters.SQL.Sandbox.allow(repo, owner_pid, child_pid, opts)
   end
 end
