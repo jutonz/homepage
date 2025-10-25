e8f6238ee46a8445d9cfacda448d7c31042c22d6
Justin Toniazzo <jt@thoughtbot.com>
2025-10-25 09:43:33 -0400

    Fix flaky awair test
    This test failed from time to time on CI. I fixed it by increasing the
    timeout and also fixing an issuew with an unrelated pubsub consumer
    which expected a `timestamp` to be present.

--- apps/client/test/client/awair/monitor_test.exs
+++ apps/client/test/client/awair/monitor_test.exs
@@ -8,10 +8,14 @@ defmodule Client.Awair.MonitorTest do
 
   test "broadcasts awair data" do
     :ok = Phoenix.PubSub.subscribe(Client.PubSub, "awair")
-    json = %{"co2" => 600}
+
+    event_body = %{
+      "co2" => 600,
+      "timestamp" => "2024-06-01T12:00:00Z"
+    }
 
     Req.Test.stub(AirData, fn %{request_path: "/air-data/latest"} = conn ->
-      Req.Test.json(conn, json)
+      Req.Test.json(conn, event_body)
     end)
 
     {:ok, pid} = GenServer.start_link(Monitor, test_server())
@@ -19,11 +23,11 @@ defmodule Client.Awair.MonitorTest do
     send(pid, :poll)
 
     event = %{
-      data: AirData.from_json(json),
+      data: AirData.from_json(event_body),
       name: "test"
     }
 
-    assert_receive ^event
+    assert_receive(^event, 1000)
   end
 
   defp test_server do
