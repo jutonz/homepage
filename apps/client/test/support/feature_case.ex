4ffab1521b15b1232eb4581e326718ab99775f7f
Justin Toniazzo <jt@thoughtbot.com>
2026-02-21 10:58:10 -0500

    Fix feature tests on CI

--- apps/client/test/support/feature_case.ex
+++ apps/client/test/support/feature_case.ex
@@ -43,12 +43,13 @@ defmodule ClientWeb.FeatureCase do
           args = capabilities.chromeOptions.args
           args = Enum.filter(args, fn arg -> String.contains?(arg, "user-agent") end)
           # Enable software rendering using SwANGLE. When using --headless, we
-          # don't get a GPU, but WebGL stuff (Three.js) needs one. I found I
-          # needed this flag to get Three.js animations to work in tests on my
-          # machine. Interestingly, this didn't seem necessary for CI. Maybe
-          # something changed in recent chrome versions?
+          # don't get a GPU, but WebGL stuff (Three.js) needs one.
+          # --use-gl=angle selects ANGLE as the GL backend; --use-angle=swiftshader
+          # tells ANGLE to use SwiftShader (software rasterizer) so WebGL works
+          # in headless CI without a real GPU.
           # https://stackoverflow.com/a/73048626
           args = ["--use-gl=angle" | args]
+          args = ["--use-angle=swiftshader" | args]
           args = ["--window-size=1920,1080" | args]
 
           args =
